137b66a39e39a64f655c2572551f147ec50ce624
Фаниль Азмагулов <fanilazmagulov@Mac-mini-Fanil.local>
2020-09-29 15:20:57 +0500

    add for and if

--- script.js
+++ script.js
@@ -8,9 +8,29 @@ const personalMovieBD = {
 	privat: false
 };
 
-const lastWatchFilm = prompt('Один из последних просмотренных фильмов?');
-const raiting = prompt('На сколько оцените его?');
+for (let i = 0; i < 2; i++) {
+	const lastWatchFilm = prompt('Один из последних просмотренных фильмов?');
+	const raiting = prompt('На сколько оцените его?');
 
-personalMovieBD.movies[lastWatchFilm] = raiting;
+	if (lastWatchFilm !== '' && raiting !== '' && lastWatchFilm !== null && raiting !== null && lastWatchFilm.length < 50) {
+		personalMovieBD.movies[lastWatchFilm] = raiting;
+		console.log('done');
+	} else {
+		console.log('error');
+		i--;
+	}
+}
 
 console.log(personalMovieBD);
+
+if (personalMovieBD.count < 10) {
+	console.log('Просмотрено довольно мало фильмов');
+} else if (personalMovieBD.count > 10 && personalMovieBD.count < 30) {
+	console.log('Вы классический зритель');
+} else if(personalMovieBD.count > 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('Произошла ошибка');
+}
+
+
